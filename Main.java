43f7e4cadfa02d5363b8aeaefe5e88a52ecb3762
Kara Schatz <schatzk@xavier.edu>
2025-12-01 13:35:58 -0500

    done

--- Main.java
+++ Main.java
@@ -12,8 +12,46 @@ import java.util.List;
 public class Main
 {
     public static void main(String[] args) {
-        String[] lockers = initLockerText();
-        // TODO
+        // this array represents the word inside each locker
+        String[] lockers = initLockerText(); 
+        // this array represents if the door is open/closed
+        boolean[] doors = new boolean[lockers.length]; 
+        // this array represents the number of times a locker is touched
+        int[] count = new int[lockers.length];
+        // simulation
+        for (int i = 1; i <= 100; i++) { // each person taking their turn
+            // i represent the ith heir
+            for (int L = 1; L <= lockers.length; L++) { // with each locker
+                // L represents the locker number
+                if (L % i == 0) { // the locker is divisible by the heir
+                    if (doors[L-1] == true) { // if the door is currently open
+                        doors[L-1] = false; // shut it
+                    } else { // the door is currently closed
+                        doors[L-1] = true; // open it
+                    }
+                    count[L-1]++;
+                }
+            }
+        }
+        
+        // now which are open at the end
+        for (int L = 1; L <= lockers.length; L++) {
+            boolean currentLocker = doors[L-1];
+            if (currentLocker) {
+                System.out.println("" + L + " " + currentLocker + " " + lockers[L-1]);
+            // } else {
+                // System.out.println("" + L + " " + currentLocker);
+            }
+        }
+        
+        // now which lockers were touched only twice...
+        for (int L = 1; L <= lockers.length; L++) {
+            int c = count[L-1];
+            if (c == 2) {
+                System.out.println(lockers[L-1]);
+            }
+        }
+        
     }
 
     private static String[] initLockerText() {
